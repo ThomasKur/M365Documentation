9e1a589b82f78bfd6bdc13e7be50ca743ca3630c
Christopher Kibble <christopher.kibble@biogen.com>
2025-01-31 11:37:03 -0500

    Added automatic token refresh when needed and clearer error message on failure.

--- PSModule/M365Documentation/Internal/Helper/Invoke-DocGraph.ps1
+++ PSModule/M365Documentation/Internal/Helper/Invoke-DocGraph.ps1
@@ -42,6 +42,30 @@ Function Invoke-DocGraph(){
         $FullUrl = "$BaseUrl$version$Path"
     }
 
+    # Make sure our token isn't about to expire before the request processes.
+    if($script:token.ExpiresOn.LocalDateTime -le $(Get-Date).AddMinutes(-1)) {
+        if($script:tokenRequest.ClientSecret) {
+            # Using PublicClient-Silent
+            try {
+                $script:token = Connect-M365Doc -ClientId $script:tokenRequest.ClientId -ClientSecret $script:tokenRequest.ClientSecret -TenantId $script:tokenRequest.TenantId
+            } catch {
+                Throw "Could not refresh token. $($_.Exception.Message)."
+            }
+        } elseif ($script.tokenRequest) {
+            # Using Interactive
+            try {
+                $script:token = Connect-M365Doc -Force
+            } catch {
+                Throw "Could not refresh token. $($_.Exception.Message)."
+            }
+        } else {
+            # Using Custom Token.
+            Throw "Custom token expiring. Please create a new token."
+        }
+    }
+
+    Write-Verbose "Current token expires $($script:token.ExpiresOn.LocalDateTime)"
+
     try{
         $header = @{Authorization = "Bearer $($script:token.AccessToken)"}
         if($AcceptLanguage){
@@ -58,12 +82,26 @@ Function Invoke-DocGraph(){
         }
     } catch {
         
+        try {
+            # See if there is a valid error message to return in the json
+            $jsonResponse = $caughtError.ErrorDetails.Message | ConvertFrom-Json
+        } catch {
+            # There was no message or it wasn't formatted as json. Throw original error.
+            $jsonResponse = @{}
+        }
+
         if($_.Exception.Response.StatusCode -eq "Forbidden"){
             throw "Used application does not have sufficiant permission to access: $FullUrl"
         } elseif ($_.Exception.Response.StatusCode -eq "NotFound" -and $_.Exception.Response.ResponseUri -like "https://graph.microsoft.com/v1.0/groups*"){
             Write-Debug "Some Profiles/Apps are assigned to groups which do no longer exist. They are not displayed in the output $($_.Exception.Response.ResponseUri)."
         }  else  {
-            Write-Error $_
+            if($jsonResponse.ErrorDetails.Message) {
+                # If there was an error we can display, show it.
+                throw $jsonResponse.ErrorDetails.Message
+            } Else {
+                # No mesasge returned from Graph, return raw error.
+                throw $_
+            }
         }
     }
 
